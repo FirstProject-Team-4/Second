23efc8c1b2a5439db89df189c46888cc4e3d1f79
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-02-08 16:10:36 +0200

    header fix

--- Tripify/src/Components/Header.tsx
+++ Tripify/src/Components/Header.tsx
@@ -4,6 +4,7 @@ export const Header = () => {
     return (
         <header>
             <NavLink to='/'>Home</NavLink>
+            <NavLink to='/home'>Home</NavLink>
             <NavLink to="/register">Register</NavLink>
             <NavLink to="/login">Login</NavLink>
         </header>);

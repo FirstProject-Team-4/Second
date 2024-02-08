e1d56ec61969d030eaf27dce5ce53065db8d6a2d
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-02-08 16:00:24 +0200

    add Components

---
+++ Tripify/src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+
+export const Header = () => {
+    return (
+        <header>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to="/register">Register</NavLink>
+            <NavLink to="/login">Login</NavLink>
+        </header>);
+}

efd63f20b01563ac7a82ee7f7af861a570e2f036
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-02-10 18:14:59 +0200

    add Like/Dislike btn

--- Tripify/src/Components/Post/Post.tsx
+++ Tripify/src/Components/Post/Post.tsx
@@ -1,28 +1,27 @@
 
 import Button from '../Button';
 import { likePost, dislikePost } from '../../Service/post-service';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { useAppContext } from '../../Context/AppContext';
 
-export default function Post({ post, togglePostLike }: { post: { id: string, title: string, content: string, createdOn: string, liked: boolean }, togglePostLike: (handle: string, id: string) => void }) {
-    const navigate = useNavigate(); // Declare the missing 'navigate' variable
+export default function Post({ post, togglePostLike }: { post: { id: string, title: string, content: string, createdOn: string, likedBy:[string] }, togglePostLike: (handle: string, id: string) => void }) {
+   
     const { userData } = useAppContext(); // Declare the missing 'userData' variable
 
     const toggleLike = async () => {
-        if (Array.isArray(post.liked) && post.liked.includes(userData!.handle)) {
-            dislikePost(userData!.handle, post.id);
+        if (post.likedBy.includes(userData.handle)) {
+            await dislikePost(userData!.handle, post.id);
         } else {
-            likePost(userData!.handle, post.id);
+            await likePost(userData!.handle, post.id);
         }
         togglePostLike(userData!.handle, post.id);
     };
 
     return (
         <div className="post">
-            <h4>{post.title} <Button onClick={toggleLike}>{Array.isArray(post.liked) && post.liked.includes(userData!.handle) ? 'Dislike' : 'Like'}</Button></h4>
+            <h4>{post.title} <Button onClick={toggleLike}>{post.likedBy.includes(userData.handle) ? 'Dislike' : 'Like'}</Button></h4>
             <p>{post.content}</p>
             <p>{new Date(post.createdOn).toLocaleDateString('bg-BG')}</p>
-            <Button onClick={() => navigate(`/posts/${post.id}`)}>View</Button> {/* Replace 'navigate' with 'navigator' */}
         </div>
     )
 }

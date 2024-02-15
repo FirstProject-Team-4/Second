44493d6574f27bde3e97bc07e89d5531087d5761
YordanAleksandrov <dani541112@gmail.com>
2024-02-15 20:44:16 +0200

    comments filter

--- Tripify/src/Components/Button/DropdownMenu.tsx
+++ Tripify/src/Components/Button/DropdownMenu.tsx
@@ -5,18 +5,28 @@ function DropdownMenu(props: any) {
 
   const handleChange = (event: any) => {
     setSelectedOption(event.target.value);
-    filter(selectedOption);
+    filter(event.target.value);
   };
 
   const filter = (option: string) => {
-    props.setArray([...props.array].sort((a: any, b: any) => a[option] - b[option]));
+    console.log(props);
+    console.log('array', props.array);
+    if (option === 'createdOn Ascending') {
+      return props.setArray([...props.array].sort((a: any, b: any) => new Date(a.createdOn).getTime() - new Date(b.createdOn).getTime()));
+    }
+    if (option === 'createdOn Descending') {
+      return props.setArray([...props.array].sort((a: any, b: any) => new Date(b.createdOn).getTime() - new Date(a.createdOn).getTime()));
+    }
+    props.setArray([...props.array].sort((a: any, b: any) => b[option] - a[option]));
   };
 
   return (
     <select value={selectedOption} onChange={handleChange}>
       <option value="">Filter</option>
-      <option value="likes">Likes</option>
+      <option  value="likes">Likes</option>
       <option value="dislikes">Dislikes</option>
+      <option value="createdOn Descending">Date ↑</option>
+      <option value="createdOn Ascending">Date ↓</option>
       {/* Add more options as needed */}
     </select>
   );
--- Tripify/src/Views/AllPosts/SinglePost-view.tsx
+++ Tripify/src/Views/AllPosts/SinglePost-view.tsx
@@ -8,44 +8,62 @@ import { useAppContext } from "../../Context/AppContext";
 import Comments from "../../Components/Post/Comments";
 import { ref, update } from "firebase/database";
 import { db } from "../../config/config-firebase";
+import DropdownMenu from "../../Components/Button/DropdownMenu";
 
 
 export default function SinglePostView() {
     const { id } = useParams()
     const [posts, setPosts] = useState<PostType[]>([]);
+    const [comments, setComments] = useState([]);
     const [comment, setComment] = useState('')
     const { userData } = useAppContext();
 
     useEffect(() => {
         if (id) {
-            getPostById(id).then((value: any) => setPosts(value));
+            (async () => {
+                const post = await getPostById(id);
+                setPosts(post || []);
+                if(post){
+                     setComments(post[0].comments);
+                    console.log(post[0].comments);
+                }
+            })();
         }
-    }, [posts])
+    }, [comment]);
+    
+    
 
-    const addCurrentComment = () => {
+    const addCurrentComment = async() => {
         if (comment.length < 1) {
             return;
         }
         addComment(posts[0].id, userData, comment,);
         if(id){
             const updatePost: { [key: string]: any } = {};
+   
             updatePost[`/posts/${id}/commentsCount`] = posts[0].commentsCount + 1;
+            posts[0].commentsCount++;
             update(ref(db),updatePost);
-            getPostById(id).then((value: any) => setPosts(value));
+            const post = await getPostById(id);
+            if(post && post[0]){ 
+                setComments(post[0].comments);
+              
+            }
         }
         setComment('');
            
      
     }
         return (
-            <div>
+           posts[0]&& <div>
                 <h1>Post</h1>
 
                 {posts[0] && <Post key={posts[0].id} post={posts[0]} setPosts={setPosts} ></Post>}
                 <input value={comment} type="text" name="comment" id="comment-input" onChange={e => setComment(e.target.value)} />
                 <Button onClick={addCurrentComment}>Add Comment</Button>
+                {posts[0]  && <DropdownMenu array={comments} setArray={setComments}></DropdownMenu>}
                 <div>
-                    {posts[0]?.comments && posts[0].comments.map((c: any) => <Comments key={c.id}  comment={c} setPosts={setPosts} post={posts[0]} />)}
+                    {comments && comments.map((c: any) => <Comments key={c.id}  comment={c} setPosts={setPosts} post={posts[0]} />)}
                 </div>
             </div>
         )
